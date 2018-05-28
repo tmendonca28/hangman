5b981054c6d934c5af77b906a6cd64b7db5a476c
tmendonca28 <tmendonca28@gmail.com>
2018-05-28 17:50:43 +0300

    Add prompts and guess checks

--- src/Hangman.java
+++ src/Hangman.java
@@ -3,5 +3,11 @@ public class Hangman {
     public static void main(String[] args) {
 
         Game game = new Game("treehouse");
+        Prompter prompter = new Prompter(game);
+        while (game.getRemainingTries() > 0) {
+            prompter.displayProgress();
+            prompter.promptForGuess();
+        }
+
     }
 }
